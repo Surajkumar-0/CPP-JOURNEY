8557ee79f84b992d23a9d1752f2f48ff1fe40470
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-09 20:00:31 +0530

    Learned C++ basics and comments.

---
+++ 01_first.cpp
@@ -0,0 +1,21 @@
+// First Program of c++ .
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! \n";
+    cout << "I am learning C++";
+    return 0;
+}
+
+// c++ single line comment..
+
+// This is a comment
+
+// C++ Multi-line Comments.
+
+/* The code below will print the words Hello World!
+   to the screen, and it is amazing */
+
