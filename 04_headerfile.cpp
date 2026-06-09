8c6af2938ca672e9f15894d5a68afa069057b68f
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-09 20:32:16 +0530

    Learned C++ header files and their types.

---
+++ 04_headerfile.cpp
@@ -0,0 +1,13 @@
+// There are two types of header files:
+// 1. System header files : It comes with the compiler
+#include <iostream>
+// 2. User defined header files: It is written by the programmer
+// #include "this.h" // --> This will produce an error if this.h is no present in the current directory
+
+using namespace std;
+
+int main()
+{
+    cout << "This is Hello world program";
+    return 0;
+}
