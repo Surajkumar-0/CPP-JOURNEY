ac687edb12bb1ec1a7ef303e6d5ad75337da92d4
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-06-09 20:30:49 +0530

    Learned C++ input/output operators.

---
+++ 03_input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<< "Enter the value num1: \n"; /* '<<' This is called Insertion operator*/
+    cin>> num1; /* '>>'  This is called Extraction operator*/
+
+    cout<< "Enter the value num2: \n"; /* '<<' This is called Insertion operator*/
+    cin>> num2; /* '>>'  This is called Extraction operator*/
+
+    cout<< "The sum is "<<num1+num2;
+    
+    return 0;
+}
